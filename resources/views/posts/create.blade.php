5009f0e69b01d8e9cbe885c0485c7467148cedb2
stanley <stanley@email.com>
2024-11-20 22:07:15 +0700

    added ui

--- resources/views/posts/create.blade.php
+++ resources/views/posts/create.blade.php
@@ -2,17 +2,20 @@
 <html>
 <head>
     <title>Create Post</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous defer"></script>
 </head>
 <body>
-    <h1>Create a New Post</h1>
+    <h1 class="text-start ms-3 mt-3">Create a New Post</h1>
 
     <form action="{{ route('user_posts.store') }}" method="POST">
         @csrf
 
-        <label for="message">Message:</label><br>
-        <textarea name="message" id="message" rows="5" cols="40" required></textarea><br><br>
-
-        <button type="submit">Submit</button>
+        <label for="message" class="ms-3">Message:</label><br>
+        <div class="form-floating mt-3 ms-2 me-5">
+            <textarea class="form-control" name="message" id="message" rows="5" cols="40" required></textarea><br><br>
+        </div>
+        <button type="submit" class="btn btn-primary ms-2 mt-3 mb-3">Submit</button>
     </form>
 </body>
 </html>
--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -6,37 +6,40 @@
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous defer"></script>
 </head>
 <body>
-    <div class="container">
+    <div class="container mt-3 mb-4">
        <h1>Green Social Homepage</h1>
     </div>
 
     <form action="{{ route('user_posts.create') }}" method="GET">
-        <button type="submit" class="btn btn-primary ms-5 mt-3">Create Post</button>
+        <button type="submit" class="btn btn-primary ms-5 mt-3 mb-3">Create Post</button>
     </form>
 
     @if ($message = Session::get('success'))
-        <p style="color: green;">{{ $message }}</p>
+        <div class="container ms-5">
+            <p style="color: green;">{{ $message }}</p>
+        </div>
     @endif
 
     @if ($posts->isEmpty())
         <p>No posts available.</p>
     @else
-        <ul>
             @foreach ($posts as $post)
-                <li>
+                <div class="container mb-3 p-3 border rounded ">
                     <p>{{ $post->message }}</p>
                     <p>Likes: {{ $post->likes }}</p>
 
                     <form action="{{ route('user_posts.destroy', $post->id) }}" method="POST" style="display: inline;">
                         @csrf
                         @method('DELETE')
-                        <button type="submit">Delete</button>
+                        <button type="submit" class="btn btn-danger">Delete</button>
                     </form>
-                </li>
+                </div>
             @endforeach
-        </ul>
 
-        {{ $posts->links('pagination::bootstrap-4') }}
+        <div class="container mt-4 ps-2 pe-2">
+            {{ $posts->links('pagination::bootstrap-5') }}
+        </div>
+        
     @endif
 </body>
 </html>
