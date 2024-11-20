ec64e483cc8e65a8e97c6b9c9c6e498b472f7616
stanley <stanley@email.com>
2024-11-20 21:51:12 +0700

    updated ui

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -2,11 +2,17 @@
 <html>
 <head>
     <title>User Posts</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous defer"></script>
 </head>
 <body>
-    <h1>All Posts</h1>
+    <div class="container">
+       <h1>Green Social Homepage</h1>
+    </div>
 
-    <a href="{{ route('user_posts.create') }}">Create New Post</a>
+    <form action="{{ route('user_posts.create') }}" method="GET">
+        <button type="submit" class="btn btn-primary ms-5 mt-3">Create Post</button>
+    </form>
 
     @if ($message = Session::get('success'))
         <p style="color: green;">{{ $message }}</p>
@@ -30,7 +36,7 @@
             @endforeach
         </ul>
 
-        {{ $posts->links() }}
+        {{ $posts->links('pagination::bootstrap-4') }}
     @endif
 </body>
 </html>
