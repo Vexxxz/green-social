e2560763927a45cbbe53a2ca1422ba9fa5f7db70
Vexxxz <72611534+Vexxxz@users.noreply.github.com>
2024-11-30 08:17:20 +0700

    Update comment.create.blade.php

--- resources/views/comment/comment.create.blade.php
+++ resources/views/comment/comment.create.blade.php
@@ -1,7 +1,20 @@
-<h1>Create New Comment for Post: {{ $post->title }}</h1>
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Create New Comment</title>
+</head>
+<body>
+    <h1>Create New Comment for Post: {{ $post->title }}</h1>
 
-<form action="{{ route('comments.store', $post) }}" method="POST">
-    @csrf
-    <textarea name="content" rows="4" required></textarea>
-    <button type="submit">Create Comment</button>
-</form>
+    <form action="{{ route('comments.store', $post) }}" method="POST">
+        @csrf
+
+        <div class="form-group">
+            <label for="content">Comment Content:</label>
+            <textarea name="content" id="content" rows="4" required></textarea>
+        </div>
+
+        <button type="submit">Create Comment</button>
+    </form>
+</body>
+</html>
